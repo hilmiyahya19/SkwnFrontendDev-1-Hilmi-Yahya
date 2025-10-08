4e27d5ddc240cf765be4babccc2161ff8f45f056
hilmiyahya19 <hilmiyahya199@gmail.com>
2025-10-08 14:15:32 +0700

    update config

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      [elemName: string]: React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
+
+// Ekspor kosong agar file dianggap module
+export {};
