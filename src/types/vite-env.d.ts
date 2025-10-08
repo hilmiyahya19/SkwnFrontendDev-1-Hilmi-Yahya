080d8c225c18275043dc75ade9902a53648fc2d4
hilmiyahya19 <hilmiyahya199@gmail.com>
2025-10-08 14:19:12 +0700

    update types

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -1,17 +1,3 @@
 /// <reference types="vite/client" />
 /// <reference types="react" />
 /// <reference types="react-dom" />
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      [elemName: string]: React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      >;
-    }
-  }
-}
-
-// Ekspor kosong agar file dianggap module
-export {};
